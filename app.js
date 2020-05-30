f862021d81be65d5aeb62f5f62e12b07a7a4395b
rachid3alaoui <rachid3alaoui@gmail.com>
2020-05-30 10:17:19 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const open = document.getElementById('open');
+const modalContainer = document.getElementById('modal-container');
+const close = document.getElementById('close');
+
+
+open.addEventListener('click', () => {
+	modalContainer.classList.add('show');
+});
+
+close.addEventListener('click', () => {
+	modalContainer.classList.remove('show');
+});
